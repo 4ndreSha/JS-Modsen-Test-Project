0c64d022883016777cbfd82e94dec6e46f87faff
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-06-26 15:00:28 +0300

    Api in progress

--- src/Components/book-card.tsx
+++ src/Components/book-card.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+const key = "AIzaSyDiXSqDNIa5NHW-DtE-4v6b_elG8lpESh8";
+
+async function getBookData() {
+    var result;
+    var userInfo = await fetch(`https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=${key}`)
+    .then(response => {
+        result = response.json();
+        return result
+    })
+    .then(response => console.log(JSON.stringify(response)))
+    return result
+}
+
 function BookCard() {
+    //var bookData = getBookData();
+    //console.log(JSON.stringify(getBookData(), null, 3));
     return <>
         <div class="book-card">
             <div class="book-card__image">book image</div>
@@ -9,7 +24,7 @@ function BookCard() {
                 <div class="book-card__category">category</div>
                 <Link class="link" to="/book">
                     <div class="book-card__name">
-                        Book name that is veeery very dhgasjdgaksjhdgajkhsg
+                        Book name that is veeeery looong
                     </div>
                 </Link>
                 <div class="book-card__author">author</div>

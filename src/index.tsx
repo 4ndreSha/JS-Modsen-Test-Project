6bac70175a93d18b3f84de4afd2f18100e8e76ed
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-07-05 13:37:14 +0300

    refactor: removed unused app module

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './App';
 import {HashRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import BookPage from './pages/BookPage';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/book/:id' element={<BookPage />} />
